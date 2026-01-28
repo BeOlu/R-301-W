b81c1cec3775df248684de0066fb5478abf3871d
Rebecca <oluwabiy@ualberta.ca>
2026-01-28 08:45:53 -0700

    Add color attribute to Shape class

--- app/src/main/java/com/example/r_301_w/Shape.java
+++ app/src/main/java/com/example/r_301_w/Shape.java
@@ -3,6 +3,6 @@ package com.example.r_301_w;
 public abstract class Shape {
     private Integer x;
     private Integer y;
-    private String color=FFFFEE58;
+    private String color="FFFFEE58";
 
 }
