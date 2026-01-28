84fa5c674f16d629141dbb3bcbccbb59a3c1c29b
R.Olu <56601728+BeOlu@users.noreply.github.com>
2026-01-28 08:32:46 -0700

    Add new fields to Shape class

--- app/src/main/java/com/example/r_301_w/Shape.java
+++ app/src/main/java/com/example/r_301_w/Shape.java
@@ -4,5 +4,8 @@ public abstract class Shape {
     private Integer x;
     private Integer y;
     private String color=FFFFEE58; 
+    String num = "123456"; 
+    Integer anum = 3244;
+    String words = "sup";
 
 }
